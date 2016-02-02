02d5409adf641613a171e2b17ecfd67ddfd97a70
Ron Seminiano <ccsronron@gmail.com>
2016-02-02 09:23:21 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>File Uploader</title>
+	<link rel="stylesheet" href="libraries/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="libraries/fontawesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<script type="text/javascript" src="libraries/jquery/jquery-1.11.3.min.js"></script>
+	<script type="text/javascript" src="libraries/jquery/jquery-migrate-1.2.1.min.js"></script>
+	<script type="text/javascript" src="libraries/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/script.js"></script>
+</head>
+<body>
+<br>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<span class="fa fa-user"></span> User
+					</div>
+				</div>
+			</div>
+			<div class="col-md-8">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						Content
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
